0d8a4441a9be6220b7e30694f386530cc64a8754
tuyojr <olutuyod@gmail.com>
2022-08-04 09:45:13 +0100

    testing objects for properties

--- testFCC.js
+++ testFCC.js
@@ -253,18 +253,150 @@ const myDog = {
 // Accessing Object Properties with Dot Notation (.)
 
 // Setup
-const testObj = {
+const testObj1 = {
     "hat": "ball-cap",
     "shirt": "jersey",
     "shoes": "cleats"
 };
 
 // Only change code below this line
-const hatValue = testObj.hat;      // Change this line
-const shirtValue = testObj.shirt;    // Change this line
+const hatValue = testObj1.hat;      // Change this line
+const shirtValue = testObj1.shirt;    // Change this line
 
 
 // Accessing Object Properties with Bracket Notation ([])
+
+// Setup
+const testObj2 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj2["an entree"];   // Change this line
+const drinkValue = testObj2["the drink"];    // Change this line
+
+
+//Accessing Object Properties with Variables
+
+// Setup
+const testObj3 = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj3[playerNumber];   // Change this line
+
+
+// Updating Object Properties
+
+// Setup
+const myDog2 = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+
+//myDog.name = "Happy Coder" (dot notation)
+myDog2["name"] = "Happy Coder" //bracket notation
+
+
+// Add New Properties to a JavaScript Object
+
+const myDog3 = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+//myDog["bark"] = "woof" (bracket notation)
+myDog3.bark = "woof" //dot notation
+
+
+//Delete Properties from a JavaScript Object
+
+// Setup
+const myDog4 = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line
+
+delete myDog4.tails
+
+
+//Using Objects for Lookups
+
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+
+    // switch(val) {
+    //   case "alpha":
+    //     result = "Adams";
+    //     break;
+    //   case "bravo":
+    //     result = "Boston";
+    //     break;
+    //   case "charlie":
+    //     result = "Chicago";
+    //     break;
+    //   case "delta":
+    //     result = "Denver";
+    //     break;
+    //   case "echo":
+    //     result = "Easy";
+    //     break;
+    //   case "foxtrot":
+    //     result = "Frank";
+    // }
+
+    const lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    }
+    result = lookup[val]
+
+    // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
+
+
+//Testing Objects for Properties
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+
+    // Only change code above this line
+}
+
+
 
 
 
