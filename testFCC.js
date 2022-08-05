b157dad9e367597ad9ad38a9ec2a3f121c57c0ec
tuyojr <olutuyod@gmail.com>
2022-08-05 22:36:32 +0100

    countdown with recursion

--- testFCC.js
+++ testFCC.js
@@ -785,7 +785,19 @@ checkSign(10);
 
 //Use Recursion to Create a Countdown
 
+// Only change code below this line
+function countdown(n) {
+    if (n < 1) {
+        return []
+    } else {
+        const countArray = countdown(n - 1)
+        countArray.unshift(n)
+        return countArray
+    }
+}
 
+console.log(countdown(5))
+// Only change code above this line
 
 
 
