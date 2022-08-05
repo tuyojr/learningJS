e41fd8f0b5627bced2c3b92d073f08bad57bf9e2
tuyojr <olutuyod@gmail.com>
2022-08-05 20:33:54 +0100

    replace loops using recursion

--- testFCC.js
+++ testFCC.js
@@ -617,6 +617,23 @@ do {
 } while (k < 5) 
 
 
+// Replace Loops using Recursion
+
+function sum(arr, n) {
+    // Only change code below this line
+
+    if (n <= 0) {
+        return 0
+    } else {
+        return sum(arr, n - 1) + arr[n - 1]
+    }
+
+    // Only change code above this line
+}
+
+console.log(sum([2,3,4,5], 3))
+
+
 
 
 
