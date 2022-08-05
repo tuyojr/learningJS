68689d1de596c7657ef8a9bb90a6a5d035dd382b
tuyojr <olutuyod@gmail.com>
2022-08-05 09:07:46 +0100

    Accessing Nested Objects

--- testFCC.js
+++ testFCC.js
@@ -424,6 +424,22 @@ const myMusic = [
 ];
 
 
+// Accessing Nested Objects
+
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];    // Changed this line
+
 
 
 
