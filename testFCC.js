b4c1f342d2bc544ae359ce416ceff003bbc931a9
tuyojr <olutuyod@gmail.com>
2022-08-05 10:23:06 +0100

    Count Backwards With a For Loop

--- testFCC.js
+++ testFCC.js
@@ -549,6 +549,17 @@ for (let i = 1; i < 10; i += 2) {
     myArray2.push(i)
 }
 
+//Count Backwards With a For Loop
+
+// Setup
+const myArray3 = [];
+
+// Only change code below this line
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+for (let i = 9; i > 0; i -= 2) {
+    myArray3.push(i)
+}
+
 
 
 
