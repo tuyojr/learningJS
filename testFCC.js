dc305d69e489a791e5aae499561babd672194357
tuyojr <olutuyod@gmail.com>
2022-08-05 10:44:36 +0100

    Iterate Through an Array with a For Loop

--- testFCC.js
+++ testFCC.js
@@ -561,6 +561,21 @@ for (let i = 9; i > 0; i -= 2) {
 }
 
 
+//Iterate Through an Array with a For Loop
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+/* Declare and initialize a variable total to 0.
+Use a for loop to add the value of each element of the myArr array to total.
+*/
+let total = 0
+
+for (let i = 0; i < myArr.length; i += 1) {
+    total += myArr[i]
+}
+
 
 
 
