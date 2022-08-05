46919bcc7aa58843d8f3c6b8e448f3c0b98c8954
tuyojr <olutuyod@gmail.com>
2022-08-05 10:58:26 +0100

    nested for loops

--- testFCC.js
+++ testFCC.js
@@ -577,6 +577,27 @@ for (let i = 0; i < myArr.length; i += 1) {
 }
 
 
+// nesting for loops
+
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    /*
+    Modify function multiplyAll so that it returns the product 
+    of all the numbers in the sub-arrays of arr.
+    */ 
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j]
+        }
+    }
+    // Only change code above this line
+    return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
 
 
 
