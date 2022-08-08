e34d9f39d2969c40025864a7f2e887739363ea13
tuyojr <olutuyod@gmail.com>
2022-08-08 08:22:03 +0100

    Compare Scopes of the var and let Keywords

--- testFCC.js
+++ testFCC.js
@@ -820,7 +820,25 @@ console.log(rangeOfNumbers(1, 5))
 // Compare Scopes of the var and let Keywords (under ECMAScript6 or ES6)
 // (ECMA = European Computer Manufacturers Association)
 
+function checkScope() {
 
+    /*
+    make i declared in the if statement is a separate variable than i declared 
+    in the first line of the function.
+    This exercise is designed to illustrate the difference between how var and 
+    let keywords assign scope to the declared variable. When programming a 
+    function similar to the one used in this exercise, it is often better to 
+    use different variable names to avoid confusion.
+    */
+
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
 
 
 
