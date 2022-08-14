f6f77343efa0dc1f6e855303daa62ece4e278050
tuyojr <olutuyod@gmail.com>
2022-08-14 10:03:54 +0100

    setting default parameters for functions

--- testFCC.js
+++ testFCC.js
@@ -905,6 +905,16 @@ const myConcat = (arr1, arr2) => {
 console.log(myConcat([1, 2], [3, 4, 5]));
 
 
+//Set Default Parameters for Your Functions
+
+// Modify the function increment by adding default parameters so 
+//that it will add 1 to number if value is not specified.
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+
 
 
 
