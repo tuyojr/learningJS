c709ce172fe294896c7e3188a9f5b4abf4f519d4
tuyojr <olutuyod@gmail.com>
2022-08-14 11:11:28 +0100

    Using Destructuring Assignment to Extract Values from Objects

--- testFCC.js
+++ testFCC.js
@@ -969,6 +969,33 @@ const { tomorrow } = HIGH_TEMPERATURES;
 
 
 
+//Use Destructuring Assignment to Assign Variables from Objects
+
+/*
+Replace the two assignments with an equivalent destructuring assignment. 
+It should still assign the variables highToday and highTomorrow the 
+values of today and tomorrow from the HIGH_TEMPERATURES object.
+*/ 
+
+const HIGH_TEMPERATURES2 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+// const highToday = HIGH_TEMPERATURES2.today;
+const { today: highToday } = HIGH_TEMPERATURES2
+
+// const highTomorrow = HIGH_TEMPERATURES2.tomorrow;
+const { tomorrow: highTomorrow } = HIGH_TEMPERATURES
+
+// Only change code above this line
+
+
+
+
 
 
 
