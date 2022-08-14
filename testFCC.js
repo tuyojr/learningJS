3d6750ce01f0fe39e0a6b14753b2747698d085b0
tuyojr <olutuyod@gmail.com>
2022-08-14 11:00:40 +0100

    Using the Spread Operator to Evaluate Arrays In-Place

--- testFCC.js
+++ testFCC.js
@@ -931,6 +931,17 @@ const sum = (...args) => {
 }
 
 
+// Use the Spread Operator to Evaluate Arrays In-Place
+
+// Copy all contents of arr1 into another array arr2 using the spread operator.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
 
 
 
