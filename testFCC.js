0017bc8f8ecf41cf8b75a010bdc7cdb42526c692
tuyojr <olutuyod@gmail.com>
2022-08-14 10:11:37 +0100

    Rest Parameter with Function Parameters

--- testFCC.js
+++ testFCC.js
@@ -915,6 +915,20 @@ const increment = (number, value = 1) => number + value;
 // Only change code above this line
 
 
+// Use the Rest Parameter with Function Parameters
+/*
+Modify the function sum using the rest parameter in such a way
+that the function sum is able to take any number of arguments 
+and return their sum.
+*/
+// const sum = (x, y, z) => {
+//   const args = [x, y, z];
+//   return args.reduce((a, b) => a + b, 0);
+// }
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
 
 
 
