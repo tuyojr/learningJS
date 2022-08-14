336b8413725a2fcda612a0506707ba06898d2de6
tuyojr <olutuyod@gmail.com>
2022-08-14 09:59:02 +0100

    writing arrow functions with parameters

--- testFCC.js
+++ testFCC.js
@@ -890,6 +890,24 @@ const magic = () => {
 };
 
 
+// Writing arrow functions with parameters
+
+// var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+// Rewrite the myConcat function which appends contents 
+//of arr2 to arr1 so that the function uses arrow function syntax.
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2)
+}
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+
+
+
 
 
 
