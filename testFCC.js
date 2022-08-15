fca45f4d3153fe0fdc0a3f58b9dc333e623a6fa1
tuyojr <olutuyod@gmail.com>
2022-08-15 21:47:13 +0100

    Using Destructuring Assignment with the Rest Parameter to Reassign Array Element

--- testFCC.js
+++ testFCC.js
@@ -1030,6 +1030,17 @@ let a = 8, b = 6;
 
 
 
+// Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+/*
+Use destructuring assignment with the rest parameter to perform an effective 
+Array.prototype.slice() so that arr is a sub-array of the original array source 
+with the first two elements omitted.
+*/
+
+
+
+
 
 
 
