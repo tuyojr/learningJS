ec7529fb3b1b22e19f46257c81040926dc6e62ab
tuyojr <olutuyod@gmail.com>
2022-08-15 21:47:46 +0100

    Using Destructuring Assignment with the Rest Parameter to Reassign Array Element

--- testFCC.js
+++ testFCC.js
@@ -1038,7 +1038,14 @@ Array.prototype.slice() so that arr is a sub-array of the original array source
 with the first two elements omitted.
 */
 
-
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a, b, ...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
 
 
 
