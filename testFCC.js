0f7052a0ee5948fb9a70e6e5b6a5bcb13b2b1b9f
tuyojr <olutuyod@gmail.com>
2022-08-16 18:18:40 +0100

    handling a rejected Promise with the catch method

--- testFCC.js
+++ testFCC.js
@@ -1343,6 +1343,33 @@ makeServerRequest2.then(result => {
 })
 
 
+// Handling a Rejected Promise with catch
+
+/*
+Add the catch method to your promise. Use error as the parameter of its callback function 
+and log error to the console.
+*/
+
+const makeServerRequest3 = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest3.then(result => {
+    console.log(result);
+});
+
+makeServerRequest3.catch(error => {
+    console.log(error)
+})
+
+
 
 
 
