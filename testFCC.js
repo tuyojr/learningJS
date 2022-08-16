161ee10945fb25d9a1de08f7adb8e00d71a605f2
tuyojr <olutuyod@gmail.com>
2022-08-16 08:38:51 +0100

    Concise Declarative Functions with ES6

--- testFCC.js
+++ testFCC.js
@@ -1122,6 +1122,33 @@ const createPerson = (name, age, gender) => {
 };
 
 
+// Write Concise Declarative Functions with ES6
+
+/*
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+Refactor the function setGear inside the object bicycle to use the shorthand 
+syntax described above.
+*/
+
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+
+
+
 
 
 
