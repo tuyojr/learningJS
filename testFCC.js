2c5a41eab7087d783a9cda5afb7118c87b7535d5
tuyojr <olutuyod@gmail.com>
2022-08-17 11:00:30 +0100

    Match Letters of the Alphabet

--- testFCC.js
+++ testFCC.js
@@ -1471,3 +1471,18 @@ let result8 = quoteSample.match(vowelRegex); // Change this line
 
 
 
+// Match Letters of the Alphabet
+
+/*
+Match all the letters in the string quoteSample.
+
+Note: Be sure to match both uppercase and lowercase letters.
+*/
+
+let quoteSample1 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result9 = quoteSample1.match(alphabetRegex); // Change this line
+
+
+
+
