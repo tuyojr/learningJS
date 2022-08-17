3396bf2ef3959fa587ae31f257fdda7b612d50d4
tuyojr <olutuyod@gmail.com>
2022-08-17 09:42:07 +0100

    Matching a single character with multiple possibilities

--- testFCC.js
+++ testFCC.js
@@ -1456,5 +1456,18 @@ let result7 = unRegex.test(exampleStr);
 
 
 
+// Matching a Single Character with Multiple Possibilities
+
+/*
+Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the 
+vowels in the string quoteSample.
+
+Note: Be sure to match both upper- and lowercase vowels.
+*/
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result8 = quoteSample.match(vowelRegex); // Change this line
+
 
 
