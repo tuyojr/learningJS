e0b2808019001604674c40fa74e09ee1c2f5d6b0
tuyojr <olutuyod@gmail.com>
2022-08-17 09:34:28 +0100

    Match Anything with Wildcard Period

--- testFCC.js
+++ testFCC.js
@@ -1443,6 +1443,17 @@ let result6 = twinkleStar.match(starRegex); // Change this line
 
 
 
+// Match Anything with Wildcard Period
+
+/*
+Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. 
+Your regex should use the wildcard character.
+*/
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result7 = unRegex.test(exampleStr);
+
 
 
 
