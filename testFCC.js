f18087d47bb091d81a384a2ee08074f9d0f5861c
tuyojr <olutuyod@gmail.com>
2022-08-22 08:16:23 +0100

    Check For Mixed Grouping of Characters

--- testFCC.js
+++ testFCC.js
@@ -1805,5 +1805,24 @@ let result28 = pwRegex.test(sampleWord);
 
 
 
+// Check For Mixed Grouping of Characters
+
+/*
+
+Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case 
+sensitive manner and it should make concessions for middle names.
+
+Then fix the code so that the regex that you have created is checked against myString and either 
+true or false is returned depending on whether the regex matches.
+*/
+
+let myString3 = "Eleanor Roosevelt";
+let myRegex5 = /(Franklin|Eleanor).*Roosevelt/; // Change this line
+let result29 = myRegex5.test(myString3); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+
+
+
+
 
 
