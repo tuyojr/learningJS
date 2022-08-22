7a9d3aeab4ca4515625f41e70d544a45b78d39b6
tuyojr <olutuyod@gmail.com>
2022-08-22 07:32:31 +0100

    Specify Only the Lower Number of Matches

--- testFCC.js
+++ testFCC.js
@@ -1746,5 +1746,18 @@ let result24 = ohRegex.test(ohStr);
 
 
 
+// Specify Only the Lower Number of Matches
+
+/*
+Change the regex haRegex to match the word Hazzah only when it has four or more letter z's.
+*/
+
+let haStr = "Hazzzzah";
+let haRegex = /haz{4,}ah/i; // Change this line
+let result25 = haRegex.test(haStr);
+
+
+
+
 
 
