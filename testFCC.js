44e93e6e4352b1572f915d2447de5b6721254239
tuyojr <olutuyod@gmail.com>
2022-08-22 09:07:41 +0100

    Use Capture Groups to Search and Replace

--- testFCC.js
+++ testFCC.js
@@ -1836,6 +1836,24 @@ let result30 = reRegex.test(repeatNum);
 
 
 
+// Use Capture Groups to Search and Replace
+
+/*
+Write a regex fixRegex using three capture groups that will search for each word in the 
+string one two three. Then update the replaceText variable to replace one two three with 
+the string three two one and assign the result to the result variable. Make sure you are 
+utilizing capture groups in the replacement string using the dollar sign ($) syntax.
+replaceText should use parenthesized submatch string(s) (i.e. the nth parenthesized 
+    submatch string, $n, corresponds to the nth capture group).
+*/
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result31 = str.replace(fixRegex, replaceText);
+
+
+
 
 
 
