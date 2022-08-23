13c7ccb0743fa37e69de9cf76725859478a07f5f
tuyojr <olutuyod@gmail.com>
2022-08-23 11:37:09 +0100

    Catch Missing Open and Closing Parenthesis After a Function Call

--- testFCC.js
+++ testFCC.js
@@ -1998,3 +1998,23 @@ console.log(result33);
 
 
 
+// Catch Missing Open and Closing Parenthesis After a Function Call
+
+/*
+Fix the code so the variable result is set to the value returned from calling 
+the function getNine.
+*/
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+}
+
+let result34 = getNine();
+console.log(result34);
+
+
+
+
+
