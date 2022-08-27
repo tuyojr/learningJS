13188add54d674262bf281cbd55c6888ffa9a880
tuyojr <olutuyod@gmail.com>
2022-08-27 07:21:37 +0100

    Use an Array to Store a Collection of Data

--- testFCC.js
+++ testFCC.js
@@ -2115,4 +2115,16 @@ console.log(myFunc())
 
 
 
+// Use an Array to Store a Collection of Data
+
+/*
+We have defined a variable called yourArray. Complete the statement by assigning an array 
+of at least 5 elements in length to the yourArray variable. Your array should contain at 
+least one string, one number, and one boolean.
+*/
+
+let yourArray = ["J.Cole", "Dave", "2pac", 8, true]; // Change this line
+
+
+
 
