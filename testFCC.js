dc4326dd1f4f2c23f35978bcbf47163a86982e45
tuyojr <olutuyod@gmail.com>
2022-08-29 09:23:05 +0100

    Copy an Array with the Spread Operator

--- testFCC.js
+++ testFCC.js
@@ -2240,3 +2240,29 @@ console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']
 
 
 
+// Copy an Array with the Spread Operator
+
+/*
+We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. 
+The function is supposed to return a new array made up of num copies of arr. We have done most of 
+the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so 
+that it works correctly (hint: another method we have already covered might come in handy here!).
+*/
+
+function copyMachine(arr6, num1) {
+    let newArr1 = [];
+    while (num1 >= 1) {
+        // Only change code below this line
+        let obj1 = [...arr6]
+        newArr1.push(obj1)
+        // Only change code above this line
+        num1--;
+    }
+    return newArr1;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+
+
+
