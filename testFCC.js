1b44fa74997dd1a5e573726655ec665b0019548d
tuyojr <olutuyod@gmail.com>
2022-08-29 09:08:51 +0100

    Copy Array Items Using slice()

--- testFCC.js
+++ testFCC.js
@@ -2220,4 +2220,23 @@ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'Pal
 
 
 
+// Copy Array Items Using slice()
+
+/*
+We have defined a function, forecast, that takes an array as an argument. Modify the function using 
+slice() to extract information from the argument array and return a new array that contains the string 
+elements warm and sunny.
+*/
+
+function forecast(arr5) {
+    // Only change code below this line
+    let newArr = arr5.slice(2, 4)
+    return newArr;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+
 
