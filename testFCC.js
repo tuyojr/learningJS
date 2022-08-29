81efb109e83499d95ebab6c54e2e9c16c876f01c
tuyojr <olutuyod@gmail.com>
2022-08-29 08:55:53 +0100

    Adding Items Using splice()

--- testFCC.js
+++ testFCC.js
@@ -2201,3 +2201,23 @@ console.log(arr3);
 
 
 
+// Adding Items Using splice()
+
+/*
+We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. 
+Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' 
+and 'BlanchedAlmond' in their respective places.
+*/
+
+function htmlColorNames(arr4) {
+    // Only change code below this line
+    arr4.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+    // Only change code above this line
+    return arr4;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+
+
