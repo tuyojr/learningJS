475ad23fdbae7f8e986af51ff86218b34b58ec3a
tuyojr <olutuyod@gmail.com>
2022-08-30 10:02:32 +0100

    Creating complex multi-dimensional arrays

--- testFCC.js
+++ testFCC.js
@@ -2337,4 +2337,44 @@ console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 
 
 
+// Creating complex multi-dimensional arrays
+
+/*
+We have defined a variable, myNestedArray, set equal to an array. 
+Modify myNestedArray, using any combination of strings, numbers, 
+and booleans for data elements, so that it has exactly five levels 
+of depth (remember, the outer-most array is level 1). Somewhere on 
+the third level, include the string deep, on the fourth level, include 
+the string deeper, and on the fifth level, include the string deepest.
+*/
+
+let myNestedArray = 
+// level 1
+[
+    // Only change code below this line
+    
+    // level 2
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    [
+        // level 3
+        ['deep'],
+        [
+            // level 4
+            ['deeper'],
+            [
+                // level 5
+                ['deepest']
+            ]
+        ]
+    ],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    ['concat', false, true, 'spread', 'array'],
+    ['mutate', 1327.98, 'splice', 'slice', 'push'],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+    // Only change code above this line
+];
+
+
+
+
 
