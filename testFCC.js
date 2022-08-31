a19f6ac86b84e1818e39b3a379f951b39036fcd7
tuyojr <olutuyod@gmail.com>
2022-08-31 09:39:07 +0100

    Adding Key-Value Pairs to JavaScript Objects

--- testFCC.js
+++ testFCC.js
@@ -2378,3 +2378,32 @@ let myNestedArray =
 
 
 
+// Adding Key-Value Pairs to JavaScript Objects
+
+/*
+A foods object has been created with three entries. Using the syntax 
+of your choice, add three more entries to it: bananas with a value of 
+13, grapes with a value of 35, and strawberries with a value of 27.
+*/
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+
+// Only change code below this line
+foods.bananas = 13
+
+const fruit = 'grapes'
+foods[fruit] = 35
+
+foods.strawberries = 27
+// Only change code above this line
+
+console.log(foods);
+
+
+
+
+
