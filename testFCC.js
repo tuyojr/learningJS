cbdb320ab6f7d2d84b658bc62d5921aafab4db42
tuyojr <olutuyod@gmail.com>
2022-09-02 10:02:19 +0100

    Generate an Array of All Object Keys with Object.keys()

--- testFCC.js
+++ testFCC.js
@@ -2588,6 +2588,42 @@ console.log(countOnline(users2));
 
 
 
+// Generate an Array of All Object Keys with Object.keys()
+
+/*
+Finish writing the getArrayOfUsers function so that it returns an array 
+containing all the properties in the object it receives as an argument.
+*/
+
+let users3 = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj1) {
+    // Only change code below this line
+    return Object.keys(obj1)
+    // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users3));
+
+
+
 
 
 
