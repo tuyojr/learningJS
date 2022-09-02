7f5c485290bf2bbc8567fe287ae6a5ee5bbbd083
tuyojr <olutuyod@gmail.com>
2022-09-02 10:10:35 +0100

    modifying an array stored in an object

--- testFCC.js
+++ testFCC.js
@@ -2624,6 +2624,49 @@ console.log(getArrayOfUsers(users3));
 
 
 
+// Modify an Array Stored in an Object
+
+/*
+Take a look at the object we've provided in the code editor. 
+The user object contains three keys. The data key contains five keys, 
+one of which contains an array of friends. From this, you can see how 
+flexible objects are as data structures. We've started writing a function 
+addFriend. Finish writing it so that it takes a user object and adds the 
+name of the friend argument to the array stored in user.data.friends and 
+returns that array.
+*/
+
+let user4 = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj1, friend) {
+    // Only change code below this line
+    user4.data.friends.push(friend)
+    return userObj1.data.friends
+    // Only change code above this line
+}
+
+console.log(addFriend(user4, 'Pete'));
+
+
+
 
 
 
