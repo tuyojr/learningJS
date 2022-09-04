da823cbfe58d98bbfcebd384e1e0c710d26bd656
tuyojr <olutuyod@gmail.com>
2022-09-04 09:57:32 +0100

    Return Largest Numbers in Arrays

--- testFCC.js
+++ testFCC.js
@@ -2862,6 +2862,56 @@ console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"
 
 
 
+// Return Largest Numbers in Arrays
+
+/*
+Return an array consisting of the largest number from each provided sub-array. 
+For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access 
+each member with array syntax arr[i].
+*/
+
+function largestOfFour(arr) {
+    // Create a variable to store the results as an array.
+    const newArr = []
+
+    // Create an outer loop to iterate through the outer array.
+    for (let k = 0; k < arr.length; k++) {
+
+        /* Create a second variable to hold the largest number and initialize 
+        it with the first number. This must be outside an inner loop so it won’t 
+        be reassigned until we find a larger number.
+        */
+        let largestNum = arr[k][0]
+
+        // Create said inner loop to work with the sub-arrays.
+        for (let b = 1; b < arr[k].length; b++) {
+
+            /*
+            Check if the element of the sub array is larger than the currently 
+            stored largest number. If so, then update the number in the variable.
+            */
+            if (arr[k][b] > largestNum) {
+                largestNum = arr[k][b]
+            }
+
+            /*
+            After the inner loop, save the largest number in the corresponding 
+            position inside of the results array
+            */
+        }
+        newArr[k] = largestNum
+    }
+    // And finally return said array.
+    return newArr;
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
+
+
 
 
 
