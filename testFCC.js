c3880f9375e6cc4bdd13daf6042b6f0966c9be97
tuyojr <olutuyod@gmail.com>
2022-09-08 07:52:43 +0100

    Slice and Splice

--- testFCC.js
+++ testFCC.js
@@ -3112,6 +3112,30 @@ Return the resulting array. The input arrays should remain the same
 after the function runs.
 */
 
+function frankenSplice(arr1, arr2, n) {
+
+    /*
+    Using the slice() function we can create an exact replica of arr2 and assign the result of the operation to a variable, newArray
+    */
+    let newArray = arr2.slice()
+
+    /*
+    Now that we have an array that we can mutate on, we can iterate through every item in the first array. For each item in the first array we can use the splice() function to insert the item into index n of newArray
+    */
+    for (let i = 0; i < arr1.length; i++) {
+        newArray.splice(n, 0, arr1[i]);
+
+        /*
+        We increment the index n by one. This will ensure that every item from the arr1 is inserted into newArray in the proper index position
+        */
+        n++;
+    }
+
+    return newArray;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
 
 
 
