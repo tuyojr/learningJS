6038a7a1f41c034b8cf2bbe5a3739aa5e07ca491
tuyojr <olutuyod@gmail.com>
2022-09-08 07:42:25 +0100

    Slice and Splice

--- testFCC.js
+++ testFCC.js
@@ -3099,6 +3099,23 @@ console.log(titleCase("I'm a little tea pot"));
 
 
 
+// Slice and Splice
+
+/*
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same 
+after the function runs.
+*/
+
+
+
+
+
 
 
 
