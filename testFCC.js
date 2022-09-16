6a9b7895e777f26661c7f4c2a42f73f11866a7b1
tuyojr <olutuyod@gmail.com>
2022-09-16 10:59:50 +0100

    creating a basic JavaScript Object

--- testFCC.js
+++ testFCC.js
@@ -3338,6 +3338,13 @@ Create a dog object with name and numLegs properties, and set
 them to a string and a number, respectively.
 */
 
+let dog = {
+    name: "Bucky",
+    numLegs: 4
+};
+
+console.log(dog.name)
+
 
 
 
