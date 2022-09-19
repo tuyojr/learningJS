bea75a5dcf866ed2cabe8788470f7d981d20d8c0
tuyojr <olutuyod@gmail.com>
2022-09-19 08:29:47 +0100

    constructor function

--- testFCC.js
+++ testFCC.js
@@ -3400,6 +3400,17 @@ console.log(dog3.sayLegs());
 
 
 
+// Define a Constructor Function
+
+/*
+Create a constructor, Dog, with properties name, color, and numLegs 
+that are set to a string, a string, and a number, respectively.
+*/
+
+
+
+
+
 
 
 
