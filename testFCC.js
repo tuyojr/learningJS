2b297307246f827979d89923e09988ab6425fe57
tuyojr <olutuyod@gmail.com>
2022-09-24 18:18:13 +0100

    defining a constructor function

--- testFCC.js
+++ testFCC.js
@@ -3407,7 +3407,11 @@ Create a constructor, Dog, with properties name, color, and numLegs
 that are set to a string, a string, and a number, respectively.
 */
 
-
+function Dog() {
+    this.name = "Zeus"
+    this.color = "grey"
+    this.numLegs = 4
+}
 
 
 
