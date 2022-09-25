4391c525e2cbee7f98ebd8617a1511efe2062197
tuyojr <olutuyod@gmail.com>
2022-09-25 09:45:57 +0100

    using 'instanceof' operator

--- testFCC.js
+++ testFCC.js
@@ -3456,6 +3456,17 @@ console.log(terrier)
 
 
 
+// Verify an Object's Constructor with instanceof
+
+/*
+Create a new instance of the House constructor, calling it myHouse
+and passing a number of bedrooms. Then, use instanceof to verify 
+that it is an instance of House.
+*/
+
+
+
+
 
 
 
