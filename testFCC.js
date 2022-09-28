f870da7992a46690068fd7069d8921dd9f2c7dd7
tuyojr <olutuyod@gmail.com>
2022-09-28 08:50:46 +0100

    iterate over all properties

--- testFCC.js
+++ testFCC.js
@@ -3528,5 +3528,16 @@ console.log(shepherd)
 
 
 
+// Iterate Over All Properties
+
+/*
+Add all of the own properties of beagle to the array ownProps. 
+Add all of the prototype properties of Dog to the array 
+prototypeProps.
+*/
+
+
+
+
 
 
