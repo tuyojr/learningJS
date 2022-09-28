369d8903e034dbd8871fd8b154b97b71d19d1001
tuyojr <olutuyod@gmail.com>
2022-09-28 09:02:56 +0100

    understanding the constructor property

--- testFCC.js
+++ testFCC.js
@@ -3560,6 +3560,16 @@ console.log(prototypeProps)
 
 
 
+// Understand the Constructor Property
+
+/*
+Write a joinDogFraternity function that takes a candidate 
+parameter and, using the constructor property, return true 
+if the candidate is a Dog, otherwise return false.
+*/
+
+
+
 
 
 
