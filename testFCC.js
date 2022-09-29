bb9d65afb003ba3e1aff9045fa03319d3fdd295a
tuyojr <olutuyod@gmail.com>
2022-09-29 07:46:28 +0100

    changing the prototype to a new object

--- testFCC.js
+++ testFCC.js
@@ -3590,6 +3590,20 @@ and describe() to the prototype of Dog by setting
 the prototype to a new object.
 */
 
+function Dog7(name) {
+    this.name = name;
+}
+
+Dog7.prototype = {
+    numLegs: 4,
+    eat: function () {
+        console.log("nom nom nom")
+    },
+    describe: function () {
+        console.log("My name is " + this.name)
+    }
+}
+
 
 
 
