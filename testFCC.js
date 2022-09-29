02721ac82202fa4021d863cc29d83bb1374e050e
tuyojr <olutuyod@gmail.com>
2022-09-29 07:32:57 +0100

    change the prototype to a new object

--- testFCC.js
+++ testFCC.js
@@ -3582,5 +3582,15 @@ function joinDogFraternity(candidate){
 
 
 
+// Change the Prototype to a New Object
+
+/*
+Add the property numLegs and the two methods eat() 
+and describe() to the prototype of Dog by setting 
+the prototype to a new object.
+*/
+
+
+
 
 
