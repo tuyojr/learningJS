baaccf9fc06dd02bf8f95bd38cef94cb22329e48
tuyojr <olutuyod@gmail.com>
2022-10-01 08:37:55 +0100

    working with inheritance

--- testFCC.js
+++ testFCC.js
@@ -3664,6 +3664,34 @@ console.log(Object.prototype.isPrototypeOf(Dog.prototype))
 // Use Inheritance So You Don't Repeat Yourself
 
 /*
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+
+};
 The eat method is repeated in both Cat and Bear. 
 Edit the code in the spirit of DRY by moving the 
 eat method to the Animal supertype.
