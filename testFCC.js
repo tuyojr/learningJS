8028cac74e07294a15e4365c3a98af4f9769d78a
tuyojr <olutuyod@gmail.com>
2022-10-01 08:41:57 +0100

    using inheritance to avoid repititve code

--- testFCC.js
+++ testFCC.js
@@ -3697,6 +3697,31 @@ Edit the code in the spirit of DRY by moving the
 eat method to the Animal supertype.
 */
 
+function Cat(name){
+    this.name = name
+}
+
+Cat.prototype = {
+    constructor: Cat,
+}
+
+function Bear(name){
+    this.name = name
+}
+
+Bear.prototype = {
+    constructor: Bear,
+}
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function(){
+        console.log("nom nom nom")
+    }
+}
+
 
 
 
