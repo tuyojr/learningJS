c482356156d35ea0a3776c2fd09430effad27a60
tuyojr <olutuyod@gmail.com>
2022-10-04 07:28:49 +0100

    using methods after inheritance

--- testFCC.js
+++ testFCC.js
@@ -3833,4 +3833,16 @@ console.log(duck1.constructor, beagle5.constructor)
 
 
 
+// Add Methods After Inheritance
+
+/*
+Add all necessary code so the Dog object inherits from 
+Animal and the Dog's prototype constructor is set to Dog. 
+Then add a bark() method to the Dog object so that beagle 
+can both eat() and bark(). The bark() method should print 
+Woof! to the console.
+*/
+
+
+
 
