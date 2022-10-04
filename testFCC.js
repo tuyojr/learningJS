8db3a705a23b82806aa325113b1a54b4a11b886b
tuyojr <olutuyod@gmail.com>
2022-10-04 15:10:53 +0100

    rewriting a function to make it an immediately invoked function expression (IIFE)

--- testFCC.js
+++ testFCC.js
@@ -3938,3 +3938,24 @@ console.log(duckie.getWeight())
 
 
 
+// Understand the Immediately Invoked Function Expression
+// (IIFE)
+
+/*
+Rewrite the function makeNest and remove its call so instead
+it's an anonymous immediately invoked function expression
+(IIFE).
+
+function makeNest(){
+    console.log("A cozy nest is ready!")
+}
+*/
+
+(function() {
+    console.log("A cozy nest is ready!")
+})()
+
+
+
+
+
