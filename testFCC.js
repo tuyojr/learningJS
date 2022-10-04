f795f7bff1b03ae1e3874d4c5e3ad1dd7585d743
tuyojr <olutuyod@gmail.com>
2022-10-04 14:49:41 +0100

    using closure to protect properties

--- testFCC.js
+++ testFCC.js
@@ -3917,3 +3917,15 @@ glideMixin(boat)
 
 
 
+/*
+Use Closure to  Protect Properties Within an Object from
+Being Modified Externally
+
+TASK:
+Change how weight is declared in the Bird function so it is
+a private variable. Then, create a method getWeight that
+returns the value of weight 15.
+*/
+
+
+
