894040c7add88d9a150e00dca92f7d8dd517c104
tuyojr <olutuyod@gmail.com>
2022-10-04 14:44:23 +0100

    using a mixin to add common behavior between two unrelated objects

--- testFCC.js
+++ testFCC.js
@@ -3896,6 +3896,24 @@ Then use the glideMixin to give both bird and boat the
 ability to glide
 */
 
+let bird1 = {
+    name: "Donald",
+    numLegs: 2
+}
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+}
+
+let glideMixin = function(obj){
+    obj.glide = function() {
+        console.log("This object glides!")
+    }
+}
+
+glideMixin(bird1)
+glideMixin(boat)
 
 
 
