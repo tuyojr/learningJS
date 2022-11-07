75a4bac7c19c3d7fb1ef090f2744aa1c9093ea79
tuyojr <olutuyod@gmail.com>
2022-11-07 10:03:59 +0100

    pass arguments to avoid external dependence in a function

--- testFCC.js
+++ testFCC.js
@@ -4255,3 +4255,27 @@ function incrementer() {
 console.log(incrementer()) // prints 5
 console.log(fixedValue) // prints 4
 
+
+
+// Pass Arguments to Avoid External Dependence in a Function
+
+/*
+Let's update the incrementer function to clearly declare its dependencies.
+
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer() {
+
+
+  // Only change code above this line
+}
+
+Write the incrementer function so it takes an argument, and then returns 
+a result after increasing the value by one.
+*/
+
+
+
+
