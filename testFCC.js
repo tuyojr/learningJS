ace0901a7c9e4e8a764dd205a64552bd090cafe8
tuyojr <olutuyod@gmail.com>
2022-11-07 11:38:20 +0100

    Refactor Global Variables Out of Functions

--- testFCC.js
+++ testFCC.js
@@ -4299,3 +4299,50 @@ function incrementer2(num) {
 console.log(incrementer2(fixedValue2)) // prints 5
 console.log(fixedValue1) // prints 4
 
+
+
+// Refactor Global Variables Out of Functions
+
+/*
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookName) {
+
+  bookList.push(bookName);
+  return bookList;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+    }
+}
+
+Rewrite the code so the global array bookList is not changed inside 
+either function. The add function should add the given bookName to 
+the end of the array passed to it and return a new array (list). 
+The remove function should remove the given bookName from the array 
+passed to it.
+
+Note: Both functions should return an array, and any new parameters 
+should be added before the bookName parameter.
+*/
+
+
+
+
+
+
+
+
+
