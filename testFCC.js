ad846d18001472fa883554bf085bac5f59d5bea6
tuyojr <olutuyod@gmail.com>
2022-11-07 08:49:44 +0100

    Avoiding Mutations and Side Effects Using Functional Programming

--- testFCC.js
+++ testFCC.js
@@ -4226,3 +4226,22 @@ const finalTabs = socialWindow
 console.log(finalTabs.tabs);
 
 
+
+
+// Avoid Mutations and Side Effects Using Functional Programming
+
+/*
+Fill in the code for the function incrementer so it returns the value of the 
+global variable fixedValue increased by one.
+
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+*/
+
