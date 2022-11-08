0c4ce9af91c04b8204c20ed2f419a1fc3655e5b4
tuyojr <olutuyod@gmail.com>
2022-11-08 10:40:31 +0100

    Return Part of an Array Using the slice Method

--- testFCC.js
+++ testFCC.js
@@ -5032,4 +5032,24 @@ console.log(new_s2)
 
 
 
+// Return Part of an Array Using the slice Method
+
+/*
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
+
+Use the slice method in the sliceArray function to return 
+part of the anim array given the provided beginSlice and 
+endSlice indices. The function should return an array.
+*/
+
+
+
 
