ec55eefbb3232a5db041ed45405c0cde90daa7b8
tuyojr <olutuyod@gmail.com>
2022-11-08 09:46:03 +0100

    Implement map on a Prototype

--- testFCC.js
+++ testFCC.js
@@ -4656,6 +4656,32 @@ console.log(JSON.stringify(ratings));
 
 
 
+// Implement map on a Prototype
+
+/*
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+Write your own Array.prototype.myMap(), which should behave 
+exactly like Array.prototype.map(). You should not use the 
+built-in map method. The Array instance can be accessed in 
+the myMap method using this.
+*/
+
+
 
 
 
