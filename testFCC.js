5c1ac827f70542ad7c7e8761a2b5cadcd38ca81a
tuyojr <olutuyod@gmail.com>
2022-11-08 11:07:22 +0100

    function to concatenate two arrays

--- testFCC.js
+++ testFCC.js
@@ -5090,4 +5090,32 @@ console.log(nonMutatingSplice(inputCities));
 
 
 
+// Combine Two Arrays Using the concat Method
+
+/*
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
+
+Use the concat method in the nonMutatingConcat function to concatenate 
+attach to the end of original. The function should return the concatenated array.
+*/
+
+function nonMutatingConcat(original, attach){
+    return original.concat(attach);
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+
+console.log(nonMutatingConcat(first, second))
+
+
 
