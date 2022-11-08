1aee9524ce408b4adeda59f4b690c8feaa37188a
tuyojr <olutuyod@gmail.com>
2022-11-08 10:33:13 +0100

    Implement the filter Method on a Prototype

--- testFCC.js
+++ testFCC.js
@@ -4976,4 +4976,32 @@ console.log(filteredList);
 
 
 
+// Implement the filter Method on a Prototype
+
+/*
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  const newArray = [];
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
+
+
+Write your own Array.prototype.myFilter(), which should behave 
+exactly like Array.prototype.filter(). You should not use the 
+built-in filter method. The Array instance can be accessed in 
+the myFilter method using this.
+
+*/
+
+
+
+
 
