3c871ea5b184d87563f20b70b449bf78d7824ed1
tuyojr <olutuyod@gmail.com>
2022-11-08 11:12:31 +0100

    Use the reduce Method to Analyze Data

--- testFCC.js
+++ testFCC.js
@@ -5148,6 +5148,21 @@ console.log(nonMutatingPush(first1, second2));
 
 
 
+// Use the reduce Method to Analyze Data
+
+/*
+{{ code is the same as watchList above }}
+The variable watchList holds an array of objects with information on several 
+movies. Use reduce to find the average IMDB rating of the movies directed 
+by Christopher Nolan. Recall from prior challenges how to filter data and 
+map over it to pull what you need. You may need to create other variables, 
+and return the average rating from getRating function. Note that the rating 
+values are saved as strings in the object and need to be converted into numbers 
+before they are used in any mathematical operations.
+*/
+
+
+
 
 
 
