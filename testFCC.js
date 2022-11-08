ab7b1554591de025ee3a1d2ca3359a4c3e400e09
tuyojr <olutuyod@gmail.com>
2022-11-08 10:55:53 +0100

    Remove Elements from an Array Using slice Instead of splice

--- testFCC.js
+++ testFCC.js
@@ -5060,4 +5060,29 @@ console.log(sliceArr(inputAnim, 1, 3));
 
 
 
+// Remove Elements from an Array Using slice Instead of splice
+
+/*
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  return cities.splice(3);
+
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
+
+Rewrite the function nonMutatingSplice by using slice instead of splice. 
+It should limit the provided cities array to a length of 3, and return a 
+new array with only the first three items.
+
+Do not mutate the original array provided to the function.
+*/
+
+
+
+
+
+
 
