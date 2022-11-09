34e4f95ccecd3c97e9899bbb8555b8e858ca8fde
tuyojr <olutuyod@gmail.com>
2022-11-09 12:31:03 +0100

    currying fucntion to add 3 arguments together

--- testFCC.js
+++ testFCC.js
@@ -5606,6 +5606,17 @@ Fill in the body of the add function so it uses currying to add parameters
 x, y, and z.
 */
 
+function add(x) {
+
+    return function (y) {
+        return function (z) {
+            return x + y + z;
+        }
+    }
+}
+
+console.log(add(10)(20)(30));
+
 
 
 
