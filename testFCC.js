5320d1bfda71ec5f42e333212ac64468d6a5784b
tuyojr <olutuyod@gmail.com>
2022-11-09 12:00:15 +0100

    Apply Functional Programming to Convert Strings to URL Slugs

--- testFCC.js
+++ testFCC.js
@@ -5494,6 +5494,32 @@ console.log(sentensify("May-the-force-be-with-you"));
 
 
 
+// Apply Functional Programming to Convert Strings to URL Slugs
+
+/*
+// Only change code below this line
+function urlSlug(title) {
+
+
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
+
+Fill in the urlSlug function so it converts a string title and returns 
+the hyphenated version for the URL. You can use any of the methods covered 
+in this section, and don't use replace. Here are the requirements:
+
+The input is a string with spaces and title-cased words
+
+The output is a string with the spaces between words replaced by a hyphen (-)
+
+The output should be all lower-cased letters
+
+The output should not have any spaces
+*/
+
+
+
 
 
 
