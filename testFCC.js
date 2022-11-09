2010cba243030c95ef7d75866e1a9e281e3c4d39
tuyojr <olutuyod@gmail.com>
2022-11-09 10:15:53 +0100

    Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- testFCC.js
+++ testFCC.js
@@ -5299,6 +5299,30 @@ console.log(getRating(watchList3));
 
 
 
+// Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+
+/*
+const squareList = arr => {
+  // Only change code below this line
+  return arr;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
+
+Complete the code for the squareList function using any combination of map(), 
+filter(), and reduce(). The function should return a new array containing the 
+squares of only the positive integers (decimal numbers are not integers) 
+when an array of real numbers is passed to it. An example of an array of 
+real numbers is [-3, 4.8, 5, 3, -3.2].
+
+Note: Your function should not use any kind of for or while loops or the 
+forEach() function.
+*/
+
+
+
 
 
 
