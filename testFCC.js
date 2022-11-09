2e775eb7562e92f6a1085753bdacadf91323b7f3
tuyojr <olutuyod@gmail.com>
2022-11-09 11:17:31 +0100

    non mutating function to sort and return an array

--- testFCC.js
+++ testFCC.js
@@ -5416,6 +5416,25 @@ of an array in ascending order. The function should return a new array,
 and not mutate the globalArray variable.
 */
 
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+
+    /*
+    A side effect of the sort method is that it changes the order of the 
+    elements in the original array. In other words, it mutates the array 
+    in place. One way to avoid this is to first concatenate an empty array 
+    to the one being sorted (remember that slice and concat return a new 
+    array), then run the sort method.
+    */
+   
+    return [].concat(arr).sort(function (a, b) {
+        return a - b
+    })
+}
+
+console.log(nonMutatingSort(globalArray));
+
 
 
 
