73eec3bdb229f77fe3624c9e211d734257399968
tuyojr <olutuyod@gmail.com>
2022-11-09 12:20:49 +0100

    Use the some Method to Check that Any Elements in an Array Meet a Criteria

--- testFCC.js
+++ testFCC.js
@@ -5563,6 +5563,25 @@ console.log(checkPositive([1, 2, 3, -4, 5]));  // false
 
 
 
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+/*
+function checkPositive(arr) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
+
+Use the some method inside the checkPositive function to check if any element 
+in arr is positive. The function should return a Boolean value.
+*/
+
+
+
+
 
 
 
