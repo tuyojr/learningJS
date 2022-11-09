be49950bad00424227270444518e8c051c8bd932
tuyojr <olutuyod@gmail.com>
2022-11-09 11:47:41 +0100

    Combine an Array into a String Using the join Method

--- testFCC.js
+++ testFCC.js
@@ -5466,6 +5466,27 @@ console.log(splitify("Hello World,I-am code"));
 
 
 
+// Combine an Array into a String Using the join Method
+
+/*
+function sentensify(str) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
+
+Use the join method (among others) inside the sentensify function to 
+make a sentence from the words in the string str. The function should 
+return a string. For example, I-like-Star-Wars would be converted to 
+I like Star Wars. For this challenge, do not use the replace method.
+*/
+
+
+
+
 
 
 
