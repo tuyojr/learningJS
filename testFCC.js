f37a165d685fd190f7c739375bf1744c34534615
tuyojr <olutuyod@gmail.com>
2022-11-09 11:19:26 +0100

    Split a String into an Array Using the split Method

--- testFCC.js
+++ testFCC.js
@@ -5437,6 +5437,26 @@ console.log(nonMutatingSort(globalArray));
 
 
 
+// Split a String into an Array Using the split Method
+
+/*
+function splitify(str) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
+
+Use the split method inside the splitify function to split str 
+into an array of words. The function should return the array. 
+Note that the words are not always separated by spaces, and the 
+array should not contain punctuation.
+*/
+
+
+
 
 
 
