3e7b5e421c9f55cf9a4919641f4c024f4caa0cb4
tuyojr <olutuyod@gmail.com>
2022-11-10 10:21:04 +0100

    Diff Two Arrays

--- testFCC.js
+++ testFCC.js
@@ -5669,6 +5669,25 @@ console.log(sumAll([1, 4]));
 
 
 
+// Diff Two Arrays
+
+/*
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+Compare two arrays and return a new array with any items only 
+found in one of the two given arrays, but not both. In other 
+words, return the symmetric difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+*/
+
+
+
 
 
 
