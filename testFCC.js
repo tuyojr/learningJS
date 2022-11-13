c3c0e0c3442cebad884303a3252e0e1429f304c4
tuyojr <olutuyod@gmail.com>
2022-11-13 11:41:39 +0100

    Everything Be True

--- testFCC.js
+++ testFCC.js
@@ -6456,6 +6456,34 @@ console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 0
 
 
 
+// Everything Be True
+
+/*
+function truthCheck(collection, pre) {
+  return pre;
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, 
+{name: "Naomi", role: "", isBot: false}, 
+{name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+
+Check if the predicate (second argument) is truthy on all 
+elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. 
+The predicate pre will be an object property and you need to 
+return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true 
+when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot 
+notation or [] notation.
+*/
+
+
+
+
 
 
 
