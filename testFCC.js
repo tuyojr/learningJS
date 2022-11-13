889e9d7511b4d8707e00fbfe58bdd5379803208f
tuyojr <olutuyod@gmail.com>
2022-11-13 10:52:04 +0100

    Sum All Primes

--- testFCC.js
+++ testFCC.js
@@ -6233,6 +6233,53 @@ console.log(sumFibs(4));
 
 
 
+// Sum All Primes
+
+/*
+function sumPrimes(num) {
+  return num;
+}
+
+sumPrimes(10);
+
+A prime number is a whole number greater than 1 with exactly two 
+divisors: 1 and itself. For example, 2 is a prime number because 
+it is only divisible by 1 and 2. In contrast, 4 is not prime 
+since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that 
+are less than or equal to num.
+*/
+
+function sumPrimes(num) {
+
+    /*
+    We loop over all values in our range, adding them to the sum if they are prime.
+  Our primality checking function returns false if the target number is divisible by any number in between 2 and the square root of the target number. We only need to check up to the square root because the square root of a number is the largest possible unique divisor.
+    */
+
+    // Helper function to check primality
+    function isPrime(num) {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Check all numbers for primality
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i))
+            sum += i;
+    }
+    return sum;
+}
+
+console.log(sumPrimes(10));
+
+
+
 
 
 
