5e4c30abbf75a7ee78f2c10411b85062cc66f375
tuyojr <olutuyod@gmail.com>
2022-11-13 11:07:52 +0100

    Drop it

--- testFCC.js
+++ testFCC.js
@@ -6344,6 +6344,26 @@ console.log(smallestCommons([1, 5]));
 
 
 
+// Drop it
+
+/*
+function dropElements(arr, func) {
+  return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
+
+Given the array arr, iterate through and remove each element 
+starting from the first element (the 0 index) until the 
+function func returns true when the iterated element is 
+passed through it.
+
+Then return the rest of the array once the condition is 
+satisfied, otherwise, arr should be returned as an empty array.
+*/
+
+
+
 
 
 
