7ba546152d6e92110c55bede241c7350216bac50
tuyojr <olutuyod@gmail.com>
2022-11-13 08:11:58 +0100

    Sorted Union

--- testFCC.js
+++ testFCC.js
@@ -6043,6 +6043,30 @@ console.log(fearNotLetter("abce"));
 
 
 
+// Sorted Union
+
+/*
+function uniteUnique(arr) {
+  return arr;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+Write a function that takes two or more arrays and returns a new 
+array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included 
+in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the 
+final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+*/
+
+
+
+
 
 
 
